9345f4ee0387ce8a38d035ea943bfcb3b8896af0
import math <129201493+CoryPearl@users.noreply.github.com>
2024-07-20 17:17:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function scrollDown(targetId) {
+  document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slideInElements = document.querySelectorAll(".slide-in");
+
+  function checkSlide() {
+    var slideInAt;
+    slideInElements.forEach((element) => {
+      if (element.id === "projects") {
+        slideInAt =
+          window.scrollY + window.innerHeight - element.clientHeight / 2 + 450;
+      } else {
+        slideInAt =
+          window.scrollY + window.innerHeight - element.clientHeight / 2;
+      }
+      const elementBottom = element.offsetTop + element.clientHeight;
+      const isHalfShown = slideInAt > element.offsetTop;
+      const isNotScrolledPast = window.scrollY < elementBottom;
+
+      if (isHalfShown && isNotScrolledPast) {
+        element.classList.add("in-view");
+      } else {
+        element.classList.remove("in-view");
+      }
+    });
+  }
+
+  checkSlide();
+
+  window.addEventListener("scroll", checkSlide);
+});
